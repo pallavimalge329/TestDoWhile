1c0be05579341a7bfdeb9e51a56c49145c8e4027
HP <HP@DESKTOP-N5QBMVO>
2021-09-01 14:19:37 +0530

    Add project files.

---
+++ TestDoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDoWhile
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            while (n <= 10);
+
+            Console.ReadLine();
+        
+         }
+
+    }
+}
